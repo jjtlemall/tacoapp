77f39390cf2a1b24a4f59ef8c69865bb4333ca9a
Ashley Shelton <63636009+jjtlemall@users.noreply.github.com>
2022-12-09 13:48:09 -0600

    UPDATED TACO2

---
+++ src/main/java/shelton/tacos2/Tacos2Application.java
@@ -0,0 +1,13 @@
+package shelton.tacos2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tacos2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tacos2Application.class, args);
+	}
+
+}
---
+++ src/test/java/shelton/tacos2/Tacos2ApplicationTests.java
@@ -0,0 +1,13 @@
+package shelton.tacos2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tacos2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
